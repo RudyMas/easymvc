d3ac0fc88e143949d6340cf2d57f292f17390269
RudyMas <rudy.mas@rudymas.be>
2016-11-22 10:38:41 +0100

    Version 0.2.1
    Bugfix:
    - Fixed the database connection error

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
  * @author      Rudy Mas <rudy.mas@rudymas.be>
  * @copyright   2016, rudymas.be. (http://www.rudymas.be/)
  * @license     https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version     0.2.0
+ * @version     0.2.1
  */
 require(__DIR__.'/vendor/autoload.php');
 require(__DIR__.'/config/config.php');
@@ -22,7 +22,8 @@ use RudyMas\PDOExt\DBconnect;
 if (USE_DATABASE) {
     require(__DIR__.'/config/database.php');
     foreach ($database as $connect) {
-        $$connect['objectName'] = new DBconnect($connect['dbHost'], $connect['dbUsername'],
+        $object = $connect['objectName'];
+        $$object = new DBconnect($connect['dbHost'], $connect['dbUsername'],
             $connect['dbPassword'], $connect['dbName'], $connect['dbCharset'], $connect['dbType']);
     }
 }
