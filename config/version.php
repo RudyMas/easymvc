5a2886e66d4f30cd27b8cfb6e763299ddb23cda1
Rudy Mas <rudy.mas@rudymas.be>
2018-08-10 18:55:49 +0200

    Version control changed

--- config/version.php
+++ config/version.php
@@ -3,6 +3,6 @@
 * Version settings for the Alpha, Beta and Production releases of the website
 */
 define('NAME_WEBSITE', 'Name Website');
-define('VERSION_ALPHA', '&alpha;0.0.1'); // Pre-Release development
-define('VERSION_BETA', '&beta;0.0.1'); // Beta-Release development
+define('VERSION_ALPHA', '0.0.1'); // Pre-Release development
+define('VERSION_BETA', '0.0.1'); // Beta-Release development
 define('VERSION', '0.0.1'); // Production-Release
