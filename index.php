ec8e56036c3d5232de43bd33ce6b4890f3ae6502
Rudy Mas <rudy.mas@rudymas.be>
2018-08-16 12:06:01 +0200

    Updated composer.json file

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  * @author      Rudy Mas <rudy.mas@rmsoft.be>
  * @copyright   2016-2018, rmsoft.be. (http://www.rmsoft.be/)
  * @license     https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version     0.7.6
+ * @version     0.7.7
  */
 session_start();
 require_once('vendor/autoload.php');
@@ -14,7 +14,7 @@ require_once('vendor/autoload.php');
 use EasyMVC\Core\Core;
 use EasyMVC\Router\Router;
 
-define('EMVC_VERSION', '0.7.6');
+define('EMVC_VERSION', '0.7.7');
 require_once('config/version.php');
 require_once('config/server.php');
 $Core = new Core();
