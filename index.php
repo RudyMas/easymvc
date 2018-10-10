97de742e2b57ee1409e9062260edc65de3b1aa74
Rudy Mas <rudy.mas@rudymas.be>
2018-10-10 08:27:01 +0200

    Added config.website.php file to the config-folder

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  * @author      Rudy Mas <rudy.mas@rmsoft.be>
  * @copyright   2016-2018, rmsoft.be. (http://www.rmsoft.be/)
  * @license     https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version     1.1.1
+ * @version     1.1.2
  */
 session_start();
 require_once('vendor/autoload.php');
@@ -14,9 +14,10 @@ require_once('vendor/autoload.php');
 use EasyMVC\Core\Core;
 use EasyMVC\Router\Router;
 
-define('EMVC_VERSION', '1.1.1');
+define('EMVC_VERSION', '1.1.2');
 require_once('config/version.php');
 require_once('config/server.php');
+require_once('config/config.website.php');
 date_default_timezone_set(TIME_ZONE);
 $Core = new Core();
 
