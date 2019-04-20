1bc61afae3df10d6bac3c845fe5aef1f04b32b88
Rudy Mas <rudy.mas@rudymas.be>
2019-04-20 13:20:26 +0200

    Updated version + Error reporting

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  * @author      Rudy Mas <rudy.mas@rmsoft.be>
  * @copyright   2016-2019, rmsoft.be. (http://www.rmsoft.be/)
  * @license     https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version     1.1.3
+ * @version     1.1.4
  */
 session_start();
 require_once('vendor/autoload.php');
@@ -14,7 +14,7 @@ require_once('vendor/autoload.php');
 use EasyMVC\Core\Core;
 use EasyMVC\Router\Router;
 
-define('EMVC_VERSION', '1.1.3');
+define('EMVC_VERSION', '1.1.4');
 require_once('config/version.php');
 require_once('config/server.php');
 require_once('config/config.website.php');
@@ -33,5 +33,7 @@ try {
     print('EasyMVC : Something went wrong.<br><br>');
     print($exception->getMessage());
     print('<br><br>');
-    print($exception->getLine());
+    print('<pre>');
+    print_r($exception);
+    print('</pre>');
 }
