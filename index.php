662169e8c367c9b08823de686ff0c730ad6b30ac
Rudy Mas <rudy.mas@rudymas.be>
2019-05-08 21:23:30 +0200

    Updated Version + Version SCSS fixed

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  * @author      Rudy Mas <rudy.mas@rmsoft.be>
  * @copyright   2016-2019, rmsoft.be. (http://www.rmsoft.be/)
  * @license     https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version     1.1.4
+ * @version     1.1.5
  */
 session_start();
 require_once('vendor/autoload.php');
@@ -14,7 +14,7 @@ require_once('vendor/autoload.php');
 use EasyMVC\Core\Core;
 use EasyMVC\Router\Router;
 
-define('EMVC_VERSION', '1.1.4');
+define('EMVC_VERSION', '1.1.5');
 require_once('config/version.php');
 require_once('config/server.php');
 require_once('config/config.website.php');
