871345775a51c2d1e483282efee0b87cdf102533
Rudy Mas <rudy.mas@rudymas.be>
2019-07-06 20:51:26 +0200

    Updated composer.json to run npm install of installation

--- index.php
+++ index.php
@@ -6,12 +6,12 @@
  * @author      Rudy Mas <rudy.mas@rmsoft.be>
  * @copyright   2016-2019, rmsoft.be. (http://www.rmsoft.be/)
  * @license     https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version     1.5.0
+ * @version     1.5.1
  */
 session_start();
 require_once('vendor/autoload.php');
 
 use EasyMVC\Core\Core;
 
-define('EMVC_VERSION', '1.5.0');
+define('EMVC_VERSION', '1.5.1');
 $Core = new Core();
