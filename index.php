c9e7387a8b2c8a095c3a254f2d8781a488ee2956
Rudy Mas <rudy.mas@rudymas.be>
2020-01-18 15:01:54 +0100

    Updated Packages

--- index.php
+++ index.php
@@ -4,14 +4,14 @@
  * An easy to use MVC PHP Framework with Mobile App Support.
  *
  * @author      Rudy Mas <rudy.mas@rmsoft.be>
- * @copyright   2016-2019, rmsoft.be. (http://www.rmsoft.be/)
+ * @copyright   2016-2020, rmsoft.be. (http://www.rmsoft.be/)
  * @license     https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version     1.5.2
+ * @version     1.5.3
  */
 session_start();
 require_once('vendor/autoload.php');
 
 use EasyMVC\Core\Core;
 
-define('EMVC_VERSION', '1.5.2');
+define('EMVC_VERSION', '1.5.3');
 $Core = new Core();
