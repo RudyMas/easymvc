f1c13c36c82a10198f041bc8df1345b0876cf738
Rudy Mas <rudy.mas@rudymas.be>
2020-05-24 18:28:57 +0200

    UPack for DBconnect

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  * @author      Rudy Mas <rudy.mas@rmsoft.be>
  * @copyright   2016-2020, rmsoft.be. (http://www.rmsoft.be/)
  * @license     https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version     1.6.0
+ * @version     1.6.1
  */
 
 use EasyMVC\Core;
@@ -15,5 +15,5 @@ session_start();
 require_once('vendor/autoload.php');
 
 
-define('EMVC_VERSION', '1.6.0');
+define('EMVC_VERSION', '1.6.1');
 $Core = new Core();
