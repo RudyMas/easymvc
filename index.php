aa87b077066da97b1bf70e39211aed09d96bec3a
Rudy Mas <rudy.mas@rudymas.be>
2020-08-01 21:14:36 +0200

    UPack for Controller

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  * @author      Rudy Mas <rudy.mas@rmsoft.be>
  * @copyright   2016-2020, rmsoft.be. (http://www.rmsoft.be/)
  * @license     https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version     1.6.2
+ * @version     1.6.3
  */
 
 use EasyMVC\Core;
@@ -15,5 +15,5 @@ session_start();
 require_once('vendor/autoload.php');
 
 
-define('EMVC_VERSION', '1.6.2');
+define('EMVC_VERSION', '1.6.3');
 $Core = new Core();
